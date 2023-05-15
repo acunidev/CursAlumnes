961422fdb74549b02eb40acbeb15721e932f50d6
Alex Cuni <82770319+acunidev@users.noreply.github.com>
2023-05-15 18:54:53 +0200

    initial commit

--- src/main/java/com/vae/cursalumnes/testControllerUno.java
+++ src/main/java/com/vae/cursalumnes/testControllerUno.java
@@ -1,2 +1,16 @@
-package com.vae.cursalumnes;public class testControllerUno {
+package com.vae.cursalumnes;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api")
+public class testControllerUno {
+
+  @GetMapping("hola")
+  public String test() {
+    return "templates/view/hola.jsp";
+  }
+
 }
